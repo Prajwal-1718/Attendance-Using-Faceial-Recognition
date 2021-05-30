c1e8bb5ef11b1323a6595becefdc23d89f82fee3
Prajwal-1718 <68771962+Prajwal-1718@users.noreply.github.com>
2021-05-30 23:24:47 +0530

    Update Attendance.py

--- Attendance.py
+++ Attendance.py
@@ -32,9 +32,11 @@ def markAttendance(name):
     with open('Attendance.csv','r+') as f:
         myDataList = f.readlines()
         nameList = []
+        #To check the names already present
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
+        #If the name is not there....
         if name not in nameList:
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
